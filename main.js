034fdeec6b61f47debdfe38b3d43c56050b6e98a
TEKT23 <iikbarrs@gmail.com>
2025-03-17 13:08:59 +0700

    Commit pertama

---
+++ main.js
@@ -0,0 +1,7 @@
+let nama = prompt("Masukkan nama Web Owner ");
+let usia = prompt("Masukkan juga usia anda sekarang ");
+alert("Selamat datang " + nama + " Di web anda");
+
+console.log(
+  "Ini adalah awal mula perjalanan kamu " + nama + " sebagai JavaScript Enginer"
+);
