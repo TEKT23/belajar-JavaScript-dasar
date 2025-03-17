9d5694d4920327af3bd8a87228c7acfe4b2f1b5c
TEKT23 <iikbarrs@gmail.com>
2025-03-17 13:16:29 +0700

    perubahan sedikit pada form intro web

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let nama = prompt("Masukkan nama Web Owner ");
 let usia = prompt("Masukkan juga usia anda sekarang ");
-alert("Selamat datang " + nama + " Di web anda");
+alert(
+  "Selamat datang " +
+    nama +
+    " Di web anda, sekarang kamu sudah berusia " +
+    usia +
+    " lohh, jangan lalai lagi"
+);
 
 console.log(
   "Ini adalah awal mula perjalanan kamu " + nama + " sebagai JavaScript Enginer"
